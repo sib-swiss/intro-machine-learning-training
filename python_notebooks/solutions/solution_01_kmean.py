6f6e2774d64691bd0efc3b6f55aa202460ac74b8
WandrilleD <wandrille.duchemin@unibas.ch>
2021-07-29 06:03:13 +0200

    finished review ch1

--- python_notebooks/solutions/solution_01_kmean.py
+++ python_notebooks/solutions/solution_01_kmean.py
@@ -1,96 +1,83 @@
-#Use the single cell dataset from [Patel et al. Science 2014](https://science.sciencemag.org/content/344/6190/1396).
-# It is composed of the expression from different cells in 5 different patients
+## 1. handle the NAs. A mean imputation should work here
 
-df_singleCell = pd.read_csv('../data/Patel.csv',sep=',', header=0 , index_col=0)
+#handling NAs
+df_mam_I = df_mam.fillna(df_mam.mean())
 
-X_singleCell = df_singleCell.T
 
-print('the data link 430 samples to 5948 genes',X_singleCell.shape)
+## 2. perform a PCA. Plot the PCA projected data as well as the weight of each column on the axes. What can you say ?
+sc = StandardScaler()
+df_mam_IS = sc.fit_transform(df_mam_I)
 
-patients = list(map(lambda s: s[0:5],df_singleCell.columns)) #take first 5 letter for patient id
+pca_mam = PCA()
 
-## here is a color code you may use for your future plots
-color_dict={'MGH26':'blue', 'MGH28':'orange', 'MGH29': 'red', 'MGH30': 'green', 'MGH31': 'pink'}
-colors=[color_dict[p] for p in patients]
+x_pca = pca_mam.fit_transform(df_mam_IS)
 
-df_singleCell.head()
+plt.subplots(figsize=(15,15))
 
-#So you have a single cell RNA seq from different patient and each patient have different cells.
+## plotting 
+sns.scatterplot( x=x_pca[:,0] , y=x_pca[:,1] , s=0)
+## adding the species name 
+for i,sp in enumerate( df_mam.index ):
+    plt.text( x_pca[i,0] , x_pca[i,1] , sp , color='blue' )
 
-#This exercise recapitulates different things we have seen until now :
 
-# 1. Plot a heatmap of the data
-# 2. Perform and plot a PCA (cell space) plot with patient color code and compare to Figure 1 in the paper
-# 3. Use t-SNE (cell space) to vizualize your data to 2 dimensions (perplexity=5), colored according to the patient color code
-# 4. Transpose the dataframe (to be on gene space) and perform k-means clustering. Find the optimal number of gene clusters.
-# 5. Vizualize the gene clusters in a t-SNE projection on the gene space 
- 
-#> The matrix `X_singleCell` we provide in the cell above corresponds to "cell x genes", meaning it is good for a cell space projection
-
-#> The last two questions may require a couple of minutes to compute solutions. 
-
-#1. Plot a heatmap of the data
-sns.heatmap(X_singleCell)
-
-# 2. Perform and plot a PCA (cell space) plot with patient color code and compare to Figure 1 in the paper
-
-## PCA
-scellPCA = PCA()
-scellPCA.fit(X_singleCell)
-x_sc_pca = scellPCA.transform(X_singleCell)
-
-## plotting - here using seaborn hue argument helps us get a nice legend easily
-sns.scatterplot( x=x_sc_pca[:,0] , y=x_sc_pca[:,1], 
-                hue=patients , palette = color_dict , s=60)
-
-var_explained = scellPCA.explained_variance_ratio_
+var_explained = pca_mam.explained_variance_ratio_
 
 plt.xlabel('First Principal Component ({0:.2f}%)'.format(var_explained[0]*100))
 plt.ylabel('Second Principal Component ({0:.2f}%)'.format(var_explained[1]*100))
 
-# 3. Use t-SNE (cell space) to vizualize your data to 2 dimensions (perplexity=5), colored according to the patient color code
-tsne_exo_sample=TSNE(n_components=2,perplexity=20).fit(X_singleCell)
+feature_vectors = pca_mam.components_.T
+arrow_size = 10
+
+# projections of the original features
+for i, v in enumerate(feature_vectors): # enumerate over the rows of feature_vectors
+    plt.arrow(0, 0, arrow_size * v[0], arrow_size * v[1], 
+              head_width=0.00008, head_length=0.00008, width=0.00005, color='k')
+    
+    text_pos = -0.005 if v[0] < 0 else 0.0001
+    
+    plt.text(v[0]*arrow_size+text_pos, 
+             v[1]*arrow_size+text_pos, 
+             df_mam.columns[i] ,fontsize=10)
+    
+
+## 3. use t-SNE to get an embedding of the data in 2D and represent it.
+##     **bonus :** plot the species names in the embedded space with `plt.text`
+
+tsne_exo_sample=TSNE(n_components=2,perplexity=20).fit(df_mam_IS)
 X_embedded_exo_sample = tsne_exo_sample.embedding_
 
 plt.figure(figsize=(10,10))
 plt.title('a point is a sample',fontsize=20)
 sns.scatterplot(X_embedded_exo_sample[:, 0], 
-                X_embedded_exo_sample[:, 1], 
-                hue=patients , palette = color_dict, s=60, lw=0)
+                X_embedded_exo_sample[:, 1],  s=0, lw=0)
+for i,sp in enumerate( df_mam.index ):
+    plt.text( X_embedded_exo_sample[i,0] , X_embedded_exo_sample[i,1] , sp , color='blue' )
+
+
 plt.xlabel('First Dimension')
 plt.ylabel('Second Dimension')
 
 
-# 4. Transpose the dataframe (to be on gene space) and perform k-means clustering (5 for 5 patients)
-X_sc_genes = X_singleCell.T
+## 4. perform a Kmean clustering on the PCA projected data. What is the best number of cluster according to the silhouette score?
+nr_clusters = np.arange(15)+2
+inertias , silhouettes = getSilhouetteProfile( x_pca , nr_clusters )
 
-pca_sc_genes = PCA() #create a PCA object
-
-pca_sc_genes.fit(X_sc_genes)
-x_pca_sc_genes = pca_sc_genes.transform(X_sc_genes)
-
-
-inertias = []
-silhouettes = []
-
-nb_clusters = np.arange(2,17)
-
-for k in nb_clusters: # testing a number of clusters from 2 to 16
-    kmeans = cluster.KMeans(k)
-    kmeans.fit(x_pca_sc_genes)
-    inertias.append(kmeans.inertia_)
-    silhouettes.append(silhouette_score(x_pca_sc_genes,kmeans.labels_))
+## getting the K with maximum silhouette
+bestI = np.argmax( silhouettes )
+bestK = nr_clusters[bestI]
+print('best K :',bestK)
 
 plt.subplots(figsize=(15,7))
 
 plt.subplot(1,2,1)
-plt.plot(nb_clusters, inertias, ls="-", lw=2)
+plt.plot(nr_clusters, inertias, ls="-", lw=2)
 plt.xlabel('Number of clusters',fontsize=20)
 plt.ylabel('Inertia',fontsize=20)
 plt.title('k-means clustering',fontsize=20)
 
 plt.subplot(1,2,2)
-plt.plot(nb_clusters, silhouettes, ls="-", lw=2)
+plt.plot(nr_clusters, silhouettes, ls="-", lw=2)
 plt.xlabel('Number of clusters',fontsize=20)
 plt.ylabel('Silhouette  score',fontsize=20)
 plt.title('k-means clustering',fontsize=20)
@@ -98,35 +85,26 @@ plt.title('k-means clustering',fontsize=20)
 plt.tight_layout()
 plt.show()
 
-bestI = np.argmax( silhouettes )
-bestK = nb_clusters[bestI]
 
-print('best number of clusters',bestK)
-
-# 5. Vizualize the gene clusters in a t-SNE projection on the gene space 
-from collections import Counter
-
-# we perform the Kmean that gave to best silhouette score
-kmeans_sc_genes = cluster.KMeans(bestK)
-kmeans_sc_genes.fit(x_pca_sc_genes)
+## 5. plot the t-SNE projected data colored according to the cluster they belong to.
+kmeans_mam = cluster.KMeans(bestK)
+kmeans_mam.fit(x_pca)
 
 # reporting the number of observations for each clusters
-cluster_labels_sc_genes = kmeans_sc_genes.labels_
-print(Counter(cluster_labels_sc_genes))
+cluster_labels_mam = kmeans_mam.labels_
+## I put here a bunch of colors I like
+cluster_2_colors = [ 'xkcd:teal' , 'xkcd:lavender' , 'xkcd:mustard' , 'xkcd:sage' ]
 
-## t-SNE embedding of the gene space data
-tsne_sc_genes=TSNE(n_components=2,perplexity=30).fit(X_sc_genes)
-X_embedded_sc_genes = tsne_sc_genes.embedding_
-
+print(Counter(cluster_labels_mam))
 
 plt.figure(figsize=(10,10))
-plt.title('t-SNE - a point is a gene',fontsize=20)
-plt.scatter(X_embedded_sc_genes[:, 0], 
-            X_embedded_sc_genes[:, 1], 
-            c=cluster_labels_sc_genes, s=60, lw=0, cmap='plasma')
+plt.title('a point is a sample',fontsize=20)
+sns.scatterplot(X_embedded_exo_sample[:, 0], 
+                X_embedded_exo_sample[:, 1],  s=0, lw=0)
+for i,sp in enumerate( df_mam.index ):
+    plt.text( X_embedded_exo_sample[i,0] , X_embedded_exo_sample[i,1] , sp , 
+             color= cluster_2_colors[cluster_labels_mam[i]] )
+
+
 plt.xlabel('First Dimension')
 plt.ylabel('Second Dimension')
-plt.xlim([-5,5])
-plt.ylim([-5,5])
-
-plt.show()
