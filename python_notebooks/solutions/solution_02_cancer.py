4438bab423e9341e3bcfe33a907c11295fa56433
WandrilleD <wandrille.duchemin@unibas.ch>
2022-09-21 14:50:46 +0200

    Ch3 cleaned + updated code and corrections

--- python_notebooks/solutions/solution_02_cancer.py
+++ python_notebooks/solutions/solution_02_cancer.py
@@ -1,4 +1,5 @@
 %%time
+
 pipe = Pipeline([('scalar',StandardScaler()),("classifier", KNeighborsClassifier())])
 # Create dictionary with candidate learning algorithms and their hyperparameters
 grid_param = [
@@ -6,33 +7,33 @@ grid_param = [
                  "classifier__n_neighbors": np.arange(1,30,5),
                  "classifier__weights": ['uniform','distance']
                  },
-                {"classifier": [LogisticRegression(n_jobs=-1,class_weight='balanced')],
+                {"classifier": [LogisticRegression(n_jobs=1,class_weight='balanced', solver='liblinear')],
                  "classifier__penalty": ['l2','l1'],
                  "classifier__C": np.logspace(-2, 2, 10)
                  },
-                {"classifier": [svm.SVC(class_weight='balanced')],
-                 "classifier__kernel": ['linear', 'rbf', 'poly'],
-                 "classifier__C":np.logspace(-6.5, -1.5, 10),
-                 "classifier__degree":np.arange(0,10,1),
-                 "classifier__gamma": np.logspace(-2,1,10)}]
+                {"classifier": [svm.SVC(class_weight='balanced', probability=True, kernel='linear')],
+                 "classifier__C":np.logspace(-6, -1, 10)},
+                {"classifier": [svm.SVC(class_weight='balanced', probability=True, kernel='rbf')],
+                 "classifier__gamma": np.logspace(-2,1,10)},
+                {"classifier": [svm.SVC(class_weight='balanced', probability=True, kernel='poly')],
+                 "classifier__C":np.logspace(-6, -1, 10),
+                 "classifier__degree":np.arange(2,10,1)}]
 
 gridsearch_C = GridSearchCV(pipe, grid_param, cv=5, verbose=0,n_jobs=-1,scoring='roc_auc') # Fit grid search
 best_model_C = gridsearch_C.fit(X_cancer_train,y_cancer_train)
 
 print(best_model_C.best_params_)
-print("Model roc_auc:",best_model_C.score(X_cancer_test,y_cancer_test))
 
-##the best model here is a SVC
+
 
 ## predicting the labels on the test set    
 y_pred_test_c=best_model_C.predict(X_cancer_test)
 
 bestC = best_model_C.best_params_['classifier__C']
-bestKernel = best_model_C.best_params_['classifier__kernel']
-bestDeg = best_model_C.best_params_['classifier__degree']
+bestPenalty = best_model_C.best_params_['classifier__penalty']
 
-plotTitle = 'SVC kernel: {} - degree: {}, C: {}\n Accuracy: {:.3f}'.format(bestKernel,
-                                                                           bestDeg,
+
+plotTitle = 'logistic regression: {} penalty ; C: {:.1e}\n Accuracy: {:.3f}'.format(bestPenalty,
                                                                          bestC,
                                                                          accuracy_score(y_cancer_test,y_pred_test_c) )
 
@@ -42,20 +43,34 @@ plotConfusionMatrix( y_cancer_test, y_pred_test_c,
                     ax = None)
 
 
-plot_roc_curve(best_model_C,X_cancer_test, y_cancer_test)
+from sklearn.metrics import RocCurveDisplay
+RocCurveDisplay.from_estimator(best_model_C,X_cancer_test, y_cancer_test)
 
 
+## solution with PCA
 %%time
-
 from sklearn.decomposition import PCA
 
-grid_param = {"classifier__kernel": ['poly'],
-                 "classifier__C":np.logspace(-2, 2, 10),
-                 "classifier__degree":np.arange(0,10,1)}
+grid_param = [
+                {"classifier": [KNeighborsClassifier(n_jobs=-1)],
+                 "classifier__n_neighbors": np.arange(1,30,5),
+                 "classifier__weights": ['uniform','distance']
+                 },
+                {"classifier": [LogisticRegression(n_jobs=1,class_weight='balanced', solver='liblinear')],
+                 "classifier__penalty": ['l2','l1'],
+                 "classifier__C": np.logspace(-2, 2, 10)
+                 },
+                {"classifier": [svm.SVC(class_weight='balanced', probability=True, kernel='linear')],
+                 "classifier__C":np.logspace(-6, -1, 10)},
+                {"classifier": [svm.SVC(class_weight='balanced', probability=True, kernel='rbf')],
+                 "classifier__gamma": np.logspace(-2,1,10)},
+                {"classifier": [svm.SVC(class_weight='balanced', probability=True, kernel='poly')],
+                 "classifier__C":np.logspace(-6, -1, 10),
+                 "classifier__degree":np.arange(2,10,1)}]
 
 
 
-PCA_NCOMPONENTS = 4
+PCA_NCOMPONENTS = 10
 
 pipe_pca = Pipeline([('scalar1',StandardScaler()),
                      ('pca',PCA(n_components=PCA_NCOMPONENTS)),
@@ -72,16 +87,13 @@ best_model_c_pca = gridsearch_c_pca.fit(X_cancer_train,y_cancer_train)
 print(best_model_c_pca.best_params_)
 print("Model accuracy:",best_model_c_pca.score(X_cancer_test,y_cancer_test))
 
-
 ## predicting the labels on the test set    
 y_pred_test_c=best_model_c_pca.predict(X_cancer_test)
 
-bestC = best_model_c_pca.best_params_['classifier__C']
-bestKernel = best_model_c_pca.best_params_['classifier__kernel']
-bestDeg = best_model_c_pca.best_params_['classifier__degree']
+bestC = best_model_C.best_params_['classifier__C']
+bestPenalty = best_model_C.best_params_['classifier__penalty']
 
-plotTitle = 'SVC kernel: {} - degree: {}, C: {}\n Accuracy: {:.3f}'.format(bestKernel,
-                                                                           bestDeg,
+plotTitle = 'logistic regression: {} penalty ; C: {:.1e}\n Accuracy: {:.3f}'.format(bestPenalty,
                                                                          bestC,
                                                                          accuracy_score(y_cancer_test,y_pred_test_c) )
 
@@ -90,7 +102,6 @@ plotConfusionMatrix( y_cancer_test, y_pred_test_c,
                     ['Benign','Malignant'] , plotTitle , 
                     ax = None)
 
-plot_roc_curve(best_model_c_pca,X_cancer_test, y_cancer_test)
 
-
-
+from sklearn.metrics import RocCurveDisplay
+RocCurveDisplay.from_estimator(best_model_C,X_cancer_test, y_cancer_test)
--- python_notebooks/solutions/solution_02_penguin.py
+++ python_notebooks/solutions/solution_02_penguin.py
@@ -6,22 +6,25 @@ grid_param = [
                  "classifier__n_neighbors": np.arange(1,30,1),
                  "classifier__weights": ['uniform','distance']
                  },
-                {"classifier": [LogisticRegression(n_jobs=-1,class_weight='balanced')],
+                {"classifier": [LogisticRegression(n_jobs=1,class_weight='balanced', solver='liblinear')],
                  "classifier__penalty": ['l2','l1'],
                  "classifier__C": np.logspace(-2, 2, 10)
                  },
-                {"classifier": [svm.SVC(class_weight='balanced')],
-                 "classifier__kernel": ['linear', 'rbf', 'poly'],
+                {"classifier": [svm.SVC(class_weight='balanced', probability=True, kernel='linear')],
+                 "classifier__C":np.logspace(-6, -1, 10)},
+                {"classifier": [svm.SVC(class_weight='balanced', probability=True, kernel='rbf')],
+                 "classifier__gamma": np.logspace(-2,1,10)},
+                {"classifier": [svm.SVC(class_weight='balanced', probability=True, kernel='poly')],
                  "classifier__C":np.logspace(-6, -1, 10),
-                 "classifier__degree":np.arange(0,10,1),
-                 "classifier__gamma": np.logspace(-2,1,10)}]
+                 "classifier__degree":np.arange(2,10,1)}]
 
 gridsearch_P = GridSearchCV(pipe, grid_param, cv=5, verbose=0,n_jobs=-1,scoring='roc_auc_ovr_weighted') # Fit grid search
 best_model_P = gridsearch_P.fit(X_penguin_train,y_penguin_train)
+print(best_model_P.best_params_)
 
 
 ## works for KNN as the best model
-print(best_model_P.best_params_)
+
 print("Model roc_auc_ovr_weighted:",best_model_P.score(X_penguin_test,y_penguin_test))
 
 y_pred_test_c=best_model_P.predict(X_penguin_test)
