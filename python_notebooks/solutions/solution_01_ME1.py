4ebc49e270079631ce531bd3e3a5e6b679878c63
WandrilleD <wandrille.duchemin@unibas.ch>
2026-04-07 13:27:26 +0200

    chapter1: small change to micro-exercise 1 + added ColumnTransformer

--- python_notebooks/solutions/solution_01_ME1.py
+++ python_notebooks/solutions/solution_01_ME1.py
@@ -1,7 +1,7 @@
 fig,ax = plt.subplots( 1,2,figsize=(15,7))
 
 sns.heatmap(X_ph_imputed.corr(),ax=ax[0], cmap='jet' , vmin = 0 , vmax = 1)
-ax[0].set_title("half-min imputed data")
+ax[0].set_title("mean imputed data")
 
 sns.heatmap(X_ph_positive.corr(), ax= ax[1], cmap='jet' , vmin = 0 , vmax = 1)
 ax[1].set_title("removed missing data")
